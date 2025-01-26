56f7d4d624197404924f499a15093e6c601ed6d2
Taylor Bryant <taylor@bryant.io>
2025-01-26 17:25:06 -0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,62 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <main className="max-w-xl w-full mx-auto px-6 py-12">
+        <h1 className="font-bold mb-2">Carucage Records</h1>
+        <p>
+          Carucage Records was an independent record label based out of St.
+          Louis, MO and Memphis, TN. Two cousins, Cory Robinson and Taylor
+          Bryant, operated the label from 2011 to 2015, releasing 10 cassette
+          tapes, 9 vinyl records, 1 CD, and 3 digital releases. You can download
+          most of our releases for free on{" "}
+          <a className="text-blue-600" href="https://music.carucage.com">
+            Bandcamp
+          </a>
+          . We no longer sell directly (which may change in the future), but you
+          may be able to find our physical releases on{" "}
+          <a
+            href="https://www.discogs.com/label/390082-Carucage-Records"
+            className="text-blue-600"
+          >
+            Discogs
+          </a>
+          .
+        </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <p className="mt-12 font-bold mb-2">Physical releases</p>
+        <ul className="space-y-1">
+          <li>CAR-001: Bigfoot - Folklore & Myth</li>
+          <li>CAR-002: Adaje / Shark Bait - Split</li>
+          <li>CAR-003: Perfect Future / Wits End - Split</li>
+          <li>CAR-004: The Anarchist Pizza Society / Circle Circle - Split</li>
+          <li>CAR-005: Innards / Two Knights - Split</li>
+          <li>CAR-006: Adaje / Lizards Have Personalities - Split</li>
+          <li>CAR-007: Innards / The Reptilian - Split</li>
+          <li>CAR-008: Dads - Brush Your Teeth, Again ;)</li>
+          <li>CAR-009: Family Might - Floor Connections</li>
+          <li>CAR-010: Tubetops - S/T</li>
+          <li>CAR-011: Loud? - S/T</li>
+          <li>CAR-012: Sailor Heart - Since the Apple Orchard</li>
+          <li>CAR-013: Foxing / Send Away Stranger - Split</li>
+          <li>CAR-014: Old Gray / Girl Scouts - Split</li>
+          <li>CAR-016: Gryscl / Coma Regalia - Split</li>
+          <li>
+            CAR-017: Joie De Vivre / The Please & Thank Yous / Emo Side Project
+            - Split
+          </li>
+          <li>CAR-018: Alta - Places</li>
+          <li>CAR-019: Shark Bait - Phantom Feelings</li>
+          <li>CAR-020: Gryscl / Weakness - Split</li>
+          <li>CAR-021: Yusuke / Delos - Split</li>
+        </ul>
+
+        <p className="mt-12 font-bold mb-2">Digital releases</p>
+        <ul className="space-y-1">
+          <li>DIGI-001: Bigfoot - Bird Song Demos</li>
+          <li>DIGI-002: Close to Me - Complete Discography</li>
+          <li>DIGI-003: Various Artists - 420 rpm</li>
+        </ul>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
